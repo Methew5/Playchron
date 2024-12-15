65e5eca0e1aaa5d643e711bb3da69df7f38ff9de
methew5 <methew.snipes@gmail.com>
2024-12-15 18:23:38 +0200

    initial commit

---
+++ app/src/main/java/com/methew5/playchron/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.methew5.playchron
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.methew5.playchron.ui.theme.PlaychronTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlaychronTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = "Playchron"
+                    )
+                }
+            }
+        }
+    }
+}
